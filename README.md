417679adcd6294c2cac50a6437c393b664d9f69c
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2023-07-23 18:32:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Apache Echarts action
+
+Render server-side Apache Echarts using GitHub Action workflow
+
+## Inputs
+
+TODO
+
+## Example usage
+
+```yaml
+uses: robiningelbrecht/apache-echarts-action
+with:
+    width: 1000
+    height: 300
+    chart-option: ''
+```
---
+++ index.js
@@ -0,0 +1,16 @@
+const core = require('@actions/core');
+const echarts = require('echarts');
+
+const width = core.getInput('width');
+const height =  core.getInput('height');
+const chartOption =  core.getInput('chart-option');
+
+const chart = echarts.init(null, null, {
+    renderer: 'svg',
+    ssr: true,
+    width: width,
+    height: height
+});
+
+chart.setOption(JSON.parse(chartOption));
+core.setOutput('svg', chart.renderToSVGString());
