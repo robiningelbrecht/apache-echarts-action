e0deb42a7c4d42dfd9344c959bf7909902593b96
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2023-07-23 18:46:33 +0200

    Debug

--- dist/index.js
+++ dist/index.js
@@ -98731,15 +98731,27 @@ const width = core.getInput('width');
 const height =  core.getInput('height');
 const chartOption =  core.getInput('chart-option');
 
-const chart = echarts.init(null, null, {
-    renderer: 'svg',
-    ssr: true,
-    width: width,
-    height: height
-});
+const run = () => {
+    core.info('Step 1');
 
-chart.setOption(JSON.parse(chartOption));
-core.setOutput('svg', chart.renderToSVGString());
+    const chart = echarts.init(null, null, {
+        renderer: 'svg',
+        ssr: true,
+        width: width,
+        height: height
+    });
+
+    core.info('Step 2');
+
+    chart.setOption(JSON.parse(chartOption));
+    core.info('Step 3');
+    core.setOutput('svg', chart.renderToSVGString());
+    core.info('Step 4');
+    process.exit(0);
+    core.info('Step 5');
+
+}
+run();
 })();
 
 module.exports = __webpack_exports__;
