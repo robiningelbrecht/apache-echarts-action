2631559623330385243631ef49083c38886f2012
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2023-07-23 18:51:20 +0200

    Testing

--- index.js
+++ index.js
@@ -6,21 +6,14 @@ const height =  core.getInput('height');
 const chartOption =  core.getInput('chart-option');
 
 const run = () => {
-    core.info('Step 1');
-
     const chart = echarts.init(null, null, {
         renderer: 'svg',
         ssr: true,
         width: width,
         height: height
     });
-
-    core.info('Step 2');
-
     chart.setOption(JSON.parse(chartOption));
-    core.info('Step 3');
     core.setOutput('svg', chart.renderToSVGString());
-    core.info('Step 4');
-
+    process.exit(0);
 }
 run();
